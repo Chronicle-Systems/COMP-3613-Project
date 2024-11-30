b261e64b34c24fcf6d5dbd26826f6cc7dfd5a005
Shane Harricharan <120674009+shxne4@users.noreply.github.com>
2024-11-30 17:09:38 -0400

    changes to admin.py

--- App/models/admin.py
+++ App/models/admin.py
@@ -1,15 +1,26 @@
 from App.database import db
-from .user import User  
+from .user import User
+import flask_login  
 from flask_login import UserMixin, login_user
-import flask_login
-from flask_login import UserMixin, login_user
-import flask_login
+
 
 class Admin(User,UserMixin):
   __tablename__ = 'admin'
 
   def login(self):
-      return flask_login.login_user(self)
+    return flask_login.login_user(self)
   
-  def __init__(self, u_ID, password, email):
-    super().__init__(u_ID, password, email)
+  def __init__(self, id, password, email):
+    super().__init__(id, password, email)
+
+  def __str__(self):
+    return f"Admin (ID={self.id}, Email={self.email})"
+  
+  def __repr__(self):
+    return f"<Admin (ID={self.id}, Email='{self.email}')>"
+  
+  def to_json(self):
+    return{
+      "admin_ID": self.id,
+      "email": self.email
+    }
