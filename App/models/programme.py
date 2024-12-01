e45ed0281925f7e85ff9544f727c3bea32e758c1
Shane Harricharan <120674009+shxne4@users.noreply.github.com>
2024-11-30 21:23:34 -0400

    Updated programme.py

--- App/models/programme.py
+++ App/models/programme.py
@@ -1,21 +1,23 @@
 from App.database import db
 
-
 class Programme(db.Model):
     __tablename__ = 'programme'
 
-    p_ID = db.Column(db.Integer, primary_key=True,
-                     nullable=False, autoincrement=True)
-    p_name = db.Column(db.String(100), nullable=False)
-    # creates reverse relationship from Programme back to Course to access courses offered in a programme
-    programmeCourses = db.relationship(
-        'CourseProgramme', backref='courses', lazy='joined')
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)  # Changed p_ID to id
+    name = db.Column(db.String(100), nullable=False, unique=True)  # Changed p_name to name and added unique constraint
+    programme_courses = db.relationship('CourseProgramme', backref='programme', lazy='joined')  # Updated relationship name
 
-    def __init__(self, p_name):
-        self.p_name = p_name
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self):
+        return f"<Programme (ID={self.id}, Name='{self.name}')>"
+
+    def __str__(self):
+        return f"Programme (ID={self.id}, Name={self.name})"
 
     def to_json(self):
         return {
-            "p_ID": self.p_ID,
-            "name": self.p_name
+            "id": self.id,  # Changed p_ID to id
+            "name": self.name
         }
